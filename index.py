f6ba8c376ffb7bac7e4205253dc1302f45d609be
CrrtN30 <andi.akbar@student.unismuh.ac.id>
2024-05-25 16:53:06 +0800

    first commit

---
+++ index.py
@@ -0,0 +1,31 @@
+import cv2 
+import numpy as np 
+
+def preprocess_image(image):
+    # Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Apply GaussianBlur
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    # Apply Canny
+    edges = cv2.Canny(blur, 50, 150) 
+    
+    return edges
+
+def blur_image(image):
+    # Apply GaussianBlur
+    blur = cv2.GaussianBlur(image, (5, 5), 0)
+    
+    black_white = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+    
+    return black_white
+
+image_original = cv2.imread('gambar.png')
+
+image_processed = preprocess_image(image_original)
+
+image_blur = blur_image(image_original)
+
+cv2.imshow('Original gambar', image_original)
+cv2.imshow('Processed gambar', image_processed)
+cv2.imshow('Blur gambar', image_blur)
+cv2.waitKey(0) #used to hold the window open until we press a key
